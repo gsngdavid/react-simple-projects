3e6aa5ce5ffd0d4a64dd560064b2a9a2880dcf7a
gsngdavid <gsngdavid@gmail.com>
2023-09-20 16:47:14 +0200

    feat: Title to the Main

---
+++ 01 - react-facts/src/components/Main.jsx
@@ -0,0 +1,7 @@
+function Main() {
+    return <div className="container py-8">
+        <h1 className="font-Inter-Bold text-[39px] text-white">Fun facts about React</h1>
+    </div>
+}
+
+export default Main;
