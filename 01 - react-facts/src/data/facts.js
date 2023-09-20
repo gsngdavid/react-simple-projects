c33b3af2557af8bf018387dd86e8df82a0c7ffce
gsngdavid <gsngdavid@gmail.com>
2023-09-20 17:31:04 +0200

    feat: Add data storage to fetch facts

---
+++ 01 - react-facts/src/data/facts.js
@@ -0,0 +1,7 @@
+export const facts = [
+    "Was first released in 2013",
+    "Was originally created by Jordan Walke",
+    "Has well over 100K stars on GitHub",
+    "Is maintained by Facebook",
+    "Powers thousands of enterprise apps, including mobile apps",
+];
