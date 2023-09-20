79ef887c75c086ba9e7155d612996193b9ce64ef
gsngdavid <gsngdavid@gmail.com>
2023-09-20 16:50:33 +0200

    feat: Add Main component in App

--- 01 - react-facts/src/App.js
+++ 01 - react-facts/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-bg-body min-h-screen">
       <Header />
+      <Main />
     </div>
   );
 }
