c14e8e74a300bdba77f82b8f26355b1fbdc813a5
gsngdavid <gsngdavid@gmail.com>
2023-09-22 10:11:14 +0200

    feat: Add photo grid

--- 02 - airbnb/src/App.js
+++ 02 - airbnb/src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
+import Photogrid from "./components/Photogrid";
 
 function App() {
   return <div className="w-full max-w-3xl mx-auto bg-white pb-32">
     <Header />
+    <Photogrid />
   </div>;
 }
 
---
+++ 02 - airbnb/src/components/Photogrid.js
@@ -0,0 +1,7 @@
+function Photogrid() {
+    return <div className="px-20 py-8">
+        <img src="./photo-grid.png" alt="Grid photoes" />
+    </div>
+}
+
+export default Photogrid;
