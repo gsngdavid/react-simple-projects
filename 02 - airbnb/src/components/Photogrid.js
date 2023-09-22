5d8cf1441855c80e478864fe201b6c15cd6d8731
gsngdavid <gsngdavid@gmail.com>
2023-09-22 10:14:29 +0200

    fix: change horizontal padding

--- 02 - airbnb/src/components/Photogrid.js
+++ 02 - airbnb/src/components/Photogrid.js
@@ -1,5 +1,5 @@
 function Photogrid() {
-    return <div className="px-20 py-8">
+    return <div className="px-4 md:px-20 py-8">
         <img src="./photo-grid.png" alt="Grid photoes" />
     </div>
 }
