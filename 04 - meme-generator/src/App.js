b0522f41092697e6ebd84d0b22ee36fbf2a78d77
gsngdavid <gsngdavid@gmail.com>
2023-09-25 12:51:25 +0200

    feat: Put inputs and fetch imaged generator button on the page

--- 04 - meme-generator/src/App.js
+++ 04 - meme-generator/src/App.js
@@ -1,8 +1,22 @@
 import Header from "./components/Header";
+import Button from "./components/UI/Button";
+import Container from "./components/UI/Container";
+import Input from "./components/UI/Input";
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Container>
+        <div className="flex flex-col gap-4 my-8">
+          <form className="grid grid-cols-2 gap-4">
+            <Input />
+            <Input />
+          </form>
+          <Button>Get a new meme image  🖼</Button>
+        </div>
+      </Container>
+    </div>
   );
 }
 
