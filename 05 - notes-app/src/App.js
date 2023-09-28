ec0cd1f4d6ab88b03c211859380df25f599aa847
gsngdavid <gsngdavid@gmail.com>
2023-09-28 15:21:35 +0200

    feat: Delete notes

--- 05 - notes-app/src/App.js
+++ 05 - notes-app/src/App.js
@@ -34,6 +34,11 @@ export default function App() {
             return updatedNotes;
         });
     }
+
+    function deleteNote(event, noteId) {
+        event.stopPropagation();
+        setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId));
+    }
     
     function findCurrentNote() {
         return notes.find(note => {
@@ -60,6 +65,7 @@ export default function App() {
                     currentNote={findCurrentNote()}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
+                    deleteNote={deleteNote}
                 />
                 {
                     currentNoteId && 
