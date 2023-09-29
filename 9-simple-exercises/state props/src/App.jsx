b7399f5e4233865a76853db3d1dfd3eeb38c6005
gsngdavid <gsngdavid@gmail.com>
2023-09-29 20:07:31 +0200

    Complete state props

--- 9-simple-exercises/state props/src/App.jsx
+++ 9-simple-exercises/state props/src/App.jsx
@@ -1,6 +1,18 @@
-function App() {
+import { useState } from "react";
 
-  return <h1>State props</h1>
+function App() {
+  const [ count, setCount ] = useState(0);
+
+  const clickHandler = () => setCount(prevCount => prevCount + 1);
+
+  return <div>
+    <div className="nav">
+      <a href="#">Home</a>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(id => <a key={id} href="#">Exercise {id}</a>)}
+    </div>
+    <p>Button has clicked {count}</p>
+    <button onClick={clickHandler}>Click me</button>
+  </div>;
 }
 
 export default App
