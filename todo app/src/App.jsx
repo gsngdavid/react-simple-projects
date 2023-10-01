d5af92409235eaadf4e1383300538f305ba65806
gsngdavid <gsngdavid@gmail.com>
2023-10-01 19:42:15 +0200

    feat: Add functions

--- todo app/src/App.jsx
+++ todo app/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import AddTodo from "./components/AddTodo"
 import Task from "./components/Task"
+import TASKS from "./data/tasks.json";
 
 function App() {
+  const [ tasks, setTasks ] = useState(TASKS);
+
+  const changeTaskStateHandler = id => {
+    setTasks(prevTasks => prevTasks.map(prevTask => prevTask.id === id ? { ...prevTask, done: !prevTask.done}: prevTask));
+  }
+
+  const deleteTaskHandler = id => {
+    setTasks(prevTasks => prevTasks.filter(prevTask => prevTask.id !== id));
+  }
+
+  const addTaskHandler = text => {
+    setTasks(prevTasks => [ ...prevTasks, { id: prevTasks.length, task: text, done: false }]);
+  }
+
   return <div>
     <h1 className="text-center text-[#EBEBEB] font-primary-bold text-8xl my-8">todos</h1>
-    <AddTodo />
+    <AddTodo onAddTask={addTaskHandler} />
 
     <div className="max-w-lg mx-auto mt-10">
-      <Task />
+      { !tasks.length && <p className="text-center">No tasks yet!</p>}
+      { tasks.map(task => <Task key={task.id} task={task} changeTaskState={changeTaskStateHandler} deleteTask={deleteTaskHandler} />) }
     </div>
   </div>
 }
