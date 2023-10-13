17d4362cabb29d95c1934cdf13cf338bfb34452f
gsngdavid <gsngdavid@gmail.com>
2023-10-13 09:06:11 +0200

    refactor: Put home page codes in a separate file

--- 9-simple-exercises/rendering json/src/App.jsx
+++ 9-simple-exercises/rendering json/src/App.jsx
@@ -1,10 +1,7 @@
-import Joke from "./components/Joke";
-import jokes from './data/jokes.json';
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="bg-[#E1F7DD] h-screen flex items-center justify-center gap-6">
-    {jokes.map(joke => <Joke key={joke.id} joke={joke} />)}
-  </div>
+  return <Home />
 }
 
 export default App
